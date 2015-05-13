80780cc59cd335f40e4c621e2c8b17fdae956cbf
Daniel Harris <bobth3bum@gmail.com>
2015-05-13 14:59:50 -0600

    change format of brewery pullout

--- app/assets/javascripts/menu_pullout.js
+++ app/assets/javascripts/menu_pullout.js
@@ -2,10 +2,12 @@ var menuPull = function(brewName) {
 
 	var menuRight = document.getElementById( 'cbp-spmenu-s2' );
 	classie.add( menuRight, 'cbp-spmenu-open' );
+	$('.brewery-name').append('<i class="fi-x"></i>');
 	$('.brewery-name').text(brewName);
 	$('.description').text("");
 	$('.address').text("");
 	$('.phone').text("");
+	$('.est').text("");
 
 	$.ajax({
 		data: {brewery_name: brewName},
@@ -19,10 +21,10 @@ var menuPull = function(brewName) {
 
 
 function addAddress(data){
-	$('.brewery-name').append("<p class='est'>" + "  est. " + data.established + "</p>");
-	$('.brewery-name').append('<img class="logo" src='+ data.logo + '></img>');
+	$('.est').append("est. " + data.established);
+	$('.logo').attr("src", data.logo);
 	$('.description').append(data.description);
 	$('.address').append(data.streetAddress + ", " + data.zipcode);
 	$('.phone').append(data.phone)
-	$('.brewery-info').html(data.streetAddress + '<br/>' + data.phone);
+	$('.brewery-info').html('<br>' + data.streetAddress + '<br/>' + data.phone);
 }
