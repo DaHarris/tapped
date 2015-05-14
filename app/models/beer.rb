d8cd9f893b7787455ee214dd76f65093570a3bfa
Daniel Harris <bobth3bum@gmail.com>
2015-05-14 15:04:05 -0600

    uniqueness of beer name validation

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,6 +4,7 @@ class Beer < ActiveRecord::Base
 
   mount_uploader :beer_pic, BeerPicUploader
   validates_presence_of :beer_name, :rating, :type_of
+  validates :beer_name, uniqueness: true
   validates :abv, :numericality => { :less_than_or_equal_to => 100 }
   validates :ibu, :numericality => true
 end
